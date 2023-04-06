123c067c734cb66c40767ea9702894b33ba337a1
6185jcaron <123214297+6185jcaron@users.noreply.github.com>
2023-04-05 18:14:29 -0700

    void die

--- birds/Assets/Scripts/Monster.cs
+++ birds/Assets/Scripts/Monster.cs
@@ -6,6 +6,24 @@ public class Monster : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        if (ShouldDieFromCollision(collision))
+        {
+            Die();
+        }
+      
+    }
+
+    private bool ShouldDieFromCollision(Collision2D collision)
+    {
         Bird bird = collision.gameObject.GetComponent<Bird>();
+        if (bird != null)
+            return true;
+
+        return false;
+    }
+
+    void Die()
+    {
+        gameObject.SetActive(false);
     }
 }
