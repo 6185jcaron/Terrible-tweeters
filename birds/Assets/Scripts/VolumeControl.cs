66cdbee35b4944a3bbb3b5ce91dabfef590d6e8b
6185jcaron <123214297+6185jcaron@users.noreply.github.com>
2023-04-13 15:50:50 -0700

    slider code

---
+++ birds/Assets/Scripts/VolumeControl.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeControl : MonoBehaviour
+{
+    [SerializeField] string _volumeParameter = "Music Volume";
+    [SerializeField] AudioMixer _mixer;
+    [SerializeField] Slider _slider;
+    [SerializeField] float _multiplier;
+
+    private void Awake()
+    {
+        _slider.onValueChanged.AddListener(HandleSliderValueChanged);
+    }
+
+    private void HandleSliderValueChanged(float value)
+    {
+        _mixer.SetFloat(_volumeParameter, Mathf.Log10(value) * _multiplier);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
